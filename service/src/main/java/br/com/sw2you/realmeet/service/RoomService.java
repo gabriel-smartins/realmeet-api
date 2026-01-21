c8298afa854a4b5ed0c52b433ae3dced822388ae
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-21 16:49:52 -0300

    refactor: organizate better packages and files

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -1,4 +1,4 @@
-package br.com.sw2you.realmeet.domain.service;
+package br.com.sw2you.realmeet.service;
 
 import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
