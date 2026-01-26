40ae05d718e6a0c8da05a30aa886f17eecf703de
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-26 13:07:54 -0300

    feat: add a creteRoom method on RoomService

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -2,27 +2,39 @@ package br.com.sw2you.realmeet.service;
 
 import static java.util.Objects.requireNonNull;
 
+import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
 import br.com.sw2you.realmeet.mapper.RoomMapper;
+import br.com.sw2you.realmeet.validator.RoomValidator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final RoomValidator roomValidator;
     private final RoomMapper roomMapper;
 
-    public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
+    public RoomService(RoomRepository roomRepository, RoomValidator roomValidator, RoomMapper roomMapper) {
         this.roomRepository = roomRepository;
+        this.roomValidator = roomValidator;
         this.roomMapper = roomMapper;
     }
 
     public RoomDTO getRoom(Long id) {
         requireNonNull(id);
         Room room = roomRepository.findByIdAndActive(id, true).orElseThrow(() -> new RoomNotFoundException("Room not Found: " + id));
+        return roomMapper.fromEntityToDTO(room);
+    }
+
+    public RoomDTO createRoom(CreateRoomDTO createRoomDTO) {
+        roomValidator.validate(createRoomDTO);
+        var room = roomMapper.fromCreateRoomDtoToEntity(createRoomDTO);
+        roomRepository.save(room);
+
         return roomMapper.fromEntityToDTO(room);
     }
 
