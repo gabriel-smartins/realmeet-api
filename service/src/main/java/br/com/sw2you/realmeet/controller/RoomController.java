7ba3019fe39c988e7a06fec09015736c7f78e4bc
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-26 13:08:36 -0300

    feat: add a creteRoom method on RoomController

--- service/src/main/java/br/com/sw2you/realmeet/controller/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/controller/RoomController.java
@@ -3,13 +3,18 @@ package br.com.sw2you.realmeet.controller;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
+import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.service.RoomService;
 import br.com.sw2you.realmeet.util.ResponseEntityUtils;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class RoomController implements RoomsApi {
@@ -23,7 +28,12 @@ public class RoomController implements RoomsApi {
     }
 
     @Override
-    public CompletableFuture<ResponseEntity<RoomDTO>> getRoom(Long id) {
+    public CompletableFuture<ResponseEntity<RoomDTO>> getRoom(@PathVariable Long id) {
         return supplyAsync(() -> roomService.getRoom(id), controllersExecutor).thenApply(ResponseEntityUtils::ok);
+    }
+
+    @Override
+    public CompletableFuture<ResponseEntity<RoomDTO>> createRoom(@Valid @RequestBody CreateRoomDTO createRoomDTO) {
+        return supplyAsync(() -> roomService.createRoom(createRoomDTO), controllersExecutor).thenApply(ResponseEntityUtils::created);
     }
 }
