f51c8bfcbc629906f66d70107be460a9fc34541e
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-26 16:27:56 -0300

    test: improve RoomValidatorUnitTest test coverage

--- service/src/test/java/br/com/sw2you/realmeet/unit/RoomValidatorUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomValidatorUnitTest.java
@@ -1,9 +1,12 @@
 package br.com.sw2you.realmeet.unit;
 
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_NAME;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newCreateRoomDTO;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
 
 import br.com.sw2you.realmeet.core.BaseUnitTest;
@@ -12,10 +15,13 @@ import br.com.sw2you.realmeet.exception.InvalidRequestException;
 import br.com.sw2you.realmeet.validator.RoomValidator;
 import br.com.sw2you.realmeet.validator.ValidationError;
 
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+
 
 public class RoomValidatorUnitTest extends BaseUnitTest {
     private RoomValidator victim;
@@ -72,5 +78,14 @@ public class RoomValidatorUnitTest extends BaseUnitTest {
 
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(new ValidationError(ROOM_SEATS, ROOM_SEATS + EXCEEDS_MAX_VALUE), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenRoomNameIsDuplicate() {
+        given(roomRepository.findByNameAndActive(DEFAULT_ROOM_NAME, true)).willReturn(Optional.of(newRoomBuilder().build()));
+
+        var exception = assertThrows(InvalidRequestException.class, () -> victim.validate(newCreateRoomDTO()));
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(new ValidationError(ROOM_NAME, ROOM_NAME + DUPLICATE), exception.getValidationErrors().getError(0));
     }
 }
