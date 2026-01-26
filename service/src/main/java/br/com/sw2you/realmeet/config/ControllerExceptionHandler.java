fbefaf4b5712cfe33dd7123a1c56b7248443b68b
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-26 15:38:44 -0300

    feat(api): improve response status on http requests

--- service/src/main/java/br/com/sw2you/realmeet/config/ControllerExceptionHandler.java
+++ service/src/main/java/br/com/sw2you/realmeet/config/ControllerExceptionHandler.java
@@ -1,10 +1,17 @@
 package br.com.sw2you.realmeet.config;
 
+import static br.com.sw2you.realmeet.util.ResponseEntityUtils.notFound;
+
 import br.com.sw2you.realmeet.api.model.ResponseError;
+import br.com.sw2you.realmeet.exception.InvalidRequestException;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -12,16 +19,18 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(RoomNotFoundException.class)
     public ResponseEntity<Object> handlerNotFoundException(Exception e) {
-        return  buildResponseEntity(HttpStatus.NOT_FOUND, e);
+        return notFound();
     }
 
-    private ResponseEntity<Object> buildResponseEntity(HttpStatus httpStatus, Exception e) {
-        return new ResponseEntity<>(
-            new ResponseError()
-                .status(httpStatus.getReasonPhrase())
-                .code(httpStatus.value()).message(e.getMessage()),
-            httpStatus
-        );
-    }
+    @ExceptionHandler(InvalidRequestException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ResponseError> handlerInvalidRequestException(InvalidRequestException e) {
 
+        return e
+                .getValidationErrors()
+                .stream()
+                .map(ex -> new ResponseError().field(ex.getField()).errorCode(ex.getErrorCode()))
+                .collect(Collectors.toList());
+    }
 }
