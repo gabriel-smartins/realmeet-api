a9177c8a0a32d38447664f374173abcf90bd0011
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-28 14:47:16 -0300

    feat: improve handler exception to AllocationController

---
+++ service/src/main/java/br/com/sw2you/realmeet/exception/AllocationCannotBeDeletedException.java
@@ -0,0 +1,7 @@
+package br.com.sw2you.realmeet.exception;
+
+public class AllocationCannotBeDeletedException extends RuntimeException {
+  public AllocationCannotBeDeletedException(String message) {
+    super(message);
+  }
+}
