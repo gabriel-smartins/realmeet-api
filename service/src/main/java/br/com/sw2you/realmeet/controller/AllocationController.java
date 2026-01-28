32a80f68544edeb0c01a3ca75b74d8b267da52a3
Gabriel Martins <gab.smartinn@outlook.com>
2026-01-28 12:09:33 -0300

    feat: add AllocationController with createAllocation method

---
+++ service/src/main/java/br/com/sw2you/realmeet/controller/AllocationController.java
@@ -0,0 +1,33 @@
+package br.com.sw2you.realmeet.controller;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import br.com.sw2you.realmeet.api.facade.AllocationsApi;
+import br.com.sw2you.realmeet.api.model.AllocationDTO;
+import br.com.sw2you.realmeet.api.model.CreateAllocationDTO;
+import br.com.sw2you.realmeet.service.AllocationService;
+import br.com.sw2you.realmeet.util.ResponseEntityUtils;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class AllocationController implements AllocationsApi {
+
+    private final Executor controllersExecutor;
+    private final AllocationService allocationService;
+
+    public AllocationController(Executor controllersExecutor, AllocationService allocationService) {
+        this.controllersExecutor = controllersExecutor;
+        this.allocationService = allocationService;
+    }
+
+    @Override
+    public CompletableFuture<ResponseEntity<AllocationDTO>> createAllocation(@Valid @RequestBody CreateAllocationDTO createAllocationDTO) {
+        return supplyAsync(() -> allocationService.createAllocation( createAllocationDTO), controllersExecutor).thenApply(ResponseEntityUtils::created);
+    }
+}
