ce62f7d1a386ff0d6b49c661a57f8a7d20cc4c99
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-03 14:49:03 -0300

    test: add a EmailConfigPropetiesIntegrationTest

---
+++ service/src/test/java/br/com/sw2you/realmeet/integration/EmailConfigPropertiesIntegrationTest.java
@@ -0,0 +1,69 @@
+package br.com.sw2you.realmeet.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import br.com.sw2you.realmeet.config.properties.EmailConfigProperties;
+import br.com.sw2you.realmeet.core.BaseIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.junit.jupiter.api.Test;
+
+public class EmailConfigPropertiesIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private EmailConfigProperties emailConfigProperties;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    @Value("${spring.mail.properties.mail.transport.protocol}")
+    private String protocol;
+
+    @Value("${spring.mail.properties.mail.smtp.port}")
+    private String port;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttlsEnabled;
+
+    @Test
+    void testLoadConfigProperties() {
+        assertNotNull(host);
+        assertEquals(host, emailConfigProperties.getHost());
+
+        assertNotNull(username);
+        assertEquals(username, emailConfigProperties.getUsername());
+
+        assertNotNull(password);
+        assertEquals(password, emailConfigProperties.getPassword());
+
+        assertNotNull(from);
+        assertEquals(from, emailConfigProperties.getFrom());
+
+        assertNotNull(protocol);
+        assertEquals(protocol, emailConfigProperties.getProperty(EmailConfigProperties.PROPERTY_TRANSPORT_PROTOCOL));
+
+        assertNotNull(port);
+        assertEquals(port, emailConfigProperties.getProperty(EmailConfigProperties.PROPERTY_SMTP_PORT));
+
+        assertNotNull(auth);
+        assertEquals(auth, emailConfigProperties.getProperty(EmailConfigProperties.PROPERTY_SMTP_AUTH));
+
+        assertNotNull(starttlsEnabled);
+        assertEquals(starttlsEnabled, emailConfigProperties.getProperty(EmailConfigProperties.PROPERTY_SMTP_STARTTLS_ENABLE));
+
+    }
+}
