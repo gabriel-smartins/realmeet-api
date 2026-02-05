bb6e13403d00cfb03dc58412b95243708ba5a3b5
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-05 12:36:33 -0300

    test: add TemplateConfigPropertiesIntegrationTest

--- service/src/main/java/br/com/sw2you/realmeet/config/properties/TemplateConfigProperties.java
+++ service/src/main/java/br/com/sw2you/realmeet/config/properties/TemplateConfigProperties.java
@@ -2,21 +2,22 @@ package br.com.sw2you.realmeet.config.properties;
 
 
 import br.com.sw2you.realmeet.config.properties.model.EmailTemplate;
+import br.com.sw2you.realmeet.email.TemplateType;
 import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
-@ConfigurationProperties(prefix = "realmeet.email.template")
+@ConfigurationProperties(prefix = "realmeet.email")
 public class TemplateConfigProperties {
 
-    private final Map<String, EmailTemplate> emailTemplateMap;
+    private final Map<String, EmailTemplate> templates;
 
     @ConstructorBinding
-    public TemplateConfigProperties(Map<String, EmailTemplate> emailTemplateMap) {
-        this.emailTemplateMap = emailTemplateMap;
+    public TemplateConfigProperties(Map<String, EmailTemplate> templates) {
+        this.templates = templates;
     }
 
-    public EmailTemplate getEmailTemplate(String property) {
-        return emailTemplateMap.get(property);
+    public EmailTemplate getEmailTemplate(TemplateType templateType) {
+        return templates.get(templateType.getTemplateName());
     }
 }
